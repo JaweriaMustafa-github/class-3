675045f8bbaa9e5b5fa23128d8a7ce55d5d88a8d
JaweriaMustafa-github <jaweriamustafa01nov@gmail.com>
2024-10-13 10:08:23 +0500

    changed no of gap

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        <ul className="flex gap-2 bg-purple-700 text-yellow-400">
+        <ul className="flex gap-5 bg-purple-700 text-yellow-400">
             <li><Link href="/">Home</Link></li>
             <li><Link href="/about">About</Link></li>
             <li><Link href="/contact-us">Contact-Us</Link></li>
